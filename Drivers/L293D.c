b43b2f04063c62649e787d83bbabadaea03811f7
bejlovba <bejlovba@rose-hulman.edu>
2022-02-12 21:40:24 -0500

    Added the L293D Driver I wrote to the driver package

---
+++ Drivers/L293D.c
@@ -0,0 +1,66 @@
+/*! \file */
+/*!
+ * L293D.c
+ *
+ * Description: L293D driver using PWM for MSP432P4111 Launchpad.
+ *              Assumes SMCLK configured for 12MHz.
+ *              Uses Timer_A1 and P2.4-7 (TA1.0-4)
+ *
+ *  Created on:
+ *      Author:Bryce Bejlovec
+ */
+
+#include <L293D.h>
+#include "msp.h"
+#include <stdint.h>
+
+void initL293D(void) {
+    /* Configure RGB LED pins */
+    P2->SEL0 |= BIT4 | BIT5 | BIT6 | BIT7;         // set 4 pins as GPIO
+    P2->SEL1 &= ~(BIT4 | BIT5 | BIT6 | BIT7);
+    P2->OUT &= ~(BIT4 | BIT5 | BIT6 | BIT7);       // clear output
+    P2->DIR |= BIT4 | BIT5 | BIT6 | BIT7;          // set as output pins
+
+
+    /* Configure Timer_A0 and CCR1/2/3 */
+    // Set period of Timer_A0 in CCR0 register for Up Mode
+    TIMER_A0->CCR[0] = PWM_PERIOD;
+    // Set initial positive pulse-width of PWM in CCR1/2/3/4 register
+    TIMER_A0->CCR[1] = 0;
+    TIMER_A0->CCR[2] = 0;
+    TIMER_A0->CCR[3] = 0;
+    TIMER_A0->CCR[4] = 0;
+    // Configure CCR1/2/3/4 for Compare mode, Reset/Set output mode, with interrupt disabled
+    TIMER_A0->CCTL[1] = TIMER_A_CCTLN_OUTMOD_7;       // Reset/Set output mode
+    TIMER_A0->CCTL[2] = TIMER_A_CCTLN_OUTMOD_7;       // Reset/Set output mode
+    TIMER_A0->CCTL[3] = TIMER_A_CCTLN_OUTMOD_7;       // Reset/Set output mode
+    TIMER_A0->CCTL[4] = TIMER_A_CCTLN_OUTMOD_7;       // Reset/Set output mode
+    // Configure Timer_A1 in Up Mode, with source SMCLK, prescale 28:1, and
+    //  interrupt disabled  -  tick rate will be (12MHz/28)
+    TIMER_A0->CTL = TIMER_A_CTL_SSEL__SMCLK |   // SMCLK
+            TIMER_A_CTL_ID__4 |                 // /4 divider
+            TIMER_A_CTL_MC__UP |                // Up mode
+            TIMER_A_CTL_CLR;                    // Clear TAR
+    TIMER_A0->EX0 = TIMER_A_EX0_IDEX__7;        // /7 divider
+}
+
+void setL293DPWM(int32_t left, int32_t right) {
+    // Update CCR1/2/3/4 registers to set new pulse-width for right and left
+    //  speed respectively.
+    //          "left" represents the rate at which the left side motors would rotate
+    //          "right" represents the rate at which the right side motors would rotate
+    if(left >= 0){
+        TIMER_A0->CCR[3] = 0;  // disable reverse
+        TIMER_A0->CCR[1] = (uint16_t)(left);    // set left speed
+    }else {
+        TIMER_A0->CCR[1] = 0;  // disable forward
+        TIMER_A0->CCR[3] = ((uint16_t)(left*(-1)-1));    // set left speed
+    }
+    if(right >= 0){
+        TIMER_A0->CCR[2] = 0;  // disable reverse
+        TIMER_A0->CCR[4] = (uint16_t)(right);    // set right speed
+    } else {
+        TIMER_A0->CCR[3] = 0;  // disable forward
+        TIMER_A0->CCR[2] = ((uint16_t)(right*(-1)-1));    // set right speed
+    }
+}
---
+++ Drivers/L293D.h
@@ -0,0 +1,42 @@
+/*
+ * L293D.h
+ *
+ *  Created on: Feb 7, 2022
+ *      Author: Bryce Bejlovec
+ */
+
+#ifndef L293D_H_
+#define L293D_H_
+#include <stdint.h>
+
+
+
+
+#endif /* L293D_H_ */
+#define PWM_PERIOD                  0x0400      // approx 50Hz signal
+
+/*!
+ * \brief This function configures pins and timer for L293D driver
+ *
+ * This function configures P2.4/5/6/7 as output pins for L293D signals
+ *  initializes Timer_A0 CCR1/2/3/4 for PWM output
+ *
+ * Modified bits 0-3 of \b P3DIR register and \b P2SEL registers.
+ * Modified \b TA0CTL, \b TA0CCTL1 and CCR registers.
+ *
+ * \return None
+ */
+extern void initL293D(void);
+
+/*!
+ * \brief This function sets color of RGB LED
+ *
+ * This function sets PWM to signed 32-bit values \a speed and direction
+ *
+ *  \param left and right signed 32-bit values
+ *
+ * Modified \b TA0CCR1/2/3/4 register.
+ *
+ * \return None
+ */
+extern void setMotorPWM(int32_t left, int32_t right);
